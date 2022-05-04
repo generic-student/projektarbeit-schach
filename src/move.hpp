be67e826e31e2a2a1b8835f570ad89aa5feb98c1
generic-student <kai.titgens@stud.th-owl.de>
2022-05-04 23:56:05 +0200

    Changed the Move struct to a bitfield to reduce its size

--- src/move.hpp
+++ src/move.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
-struct Move {
-	int startCol;
-	int startRow;
-	int targetCol;
-	int targetRow;
+namespace sm
+{
+	struct Move
+	{
+		//define a constructor because default values cannot be set in bitfields
+		Move() {
+			capture = false;
+			promotion = '\0';
+		}
 
-	bool capture = false;
-	int captureRow;
-	int captureCol;
+		unsigned short startCol : 4;
+		unsigned short startRow : 4;
+		unsigned short targetCol : 4;
+		unsigned short targetRow : 4;
 
+		bool capture : 8; // will be set to use 8bits because the struct would be padded with the remaining bits anyways
+		unsigned short captureRow : 4;
+		unsigned short captureCol : 4;
 
-	char promotion = '\0';
-};
+		char promotion : 8;
+	};
+
+}
